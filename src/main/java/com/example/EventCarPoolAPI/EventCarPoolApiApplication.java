64b34513d4f8a318dbabb7e3cb36320a8d927756
pleonasmen <lindlinus@gmail.com>
2019-08-27 10:48:21 +0200

    Started Spring Boot Project

---
+++ src/main/java/com/example/EventCarPoolAPI/EventCarPoolApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.EventCarPoolAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCarPoolApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCarPoolApiApplication.class, args);
+	}
+
+}
