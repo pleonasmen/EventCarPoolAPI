8845bfac6bff585022f4654570f7b4de01c5fd20
Johan Möller <johanmoller91@gmail.com>
2019-08-30 16:09:30 +0200

    Jag och Jocke fixade så man kan ge reference

--- src/main/java/com/example/EventCarPoolAPI/ParseJsonJohan.java
+++ src/main/java/com/example/EventCarPoolAPI/ParseJsonJohan.java
@@ -1,24 +1,20 @@
 package com.example.EventCarPoolAPI;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-
-
 @Service
 public class ParseJsonJohan {
-
     @Autowired
     PasswordEncoder encoder;
-
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    JourneyRepository journeyRepository;
     public Journey parseJson(String journey)throws JSONException {
         ArrayList<String> elementValues = parseAllElements(journey);
-
         // assigning values
         String driverId = elementValues.get(0);
         Long matchId = Long.parseLong((elementValues.get(1)));
@@ -29,14 +25,10 @@ public class ParseJsonJohan {
         LocalDate createdDate = LocalDate.now();
         Integer contributionPerHead = Integer.parseInt(elementValues.get(6));
         String tripType = elementValues.get(7);
-
         return new Journey(Long.valueOf(driverId), matchId, seats, fromCity, toCity, startTime, createdDate, contributionPerHead, tripType );
     }
-
     public User parseJsonUser(String user)throws JSONException {
-
         ArrayList<String> elementValues = parseAllElements(user);
-
         // assigning values
         String firstName = elementValues.get(0);
         String lastName = elementValues.get(1);
@@ -47,29 +39,54 @@ public class ParseJsonJohan {
         String phoneNumber =  elementValues.get(6);
         Long favouriteTeamId = Long.valueOf(elementValues.get(7));
         String role = "USER";
-
         return new User(firstName, lastName, username, password, gender, email, phoneNumber, favouriteTeamId, role);
     }
-
-
-
-
+    public Post parseJsonPost(String post)throws JSONException {
+        ArrayList<String> elementValues = parseAllElements(post);
+        System.out.println(elementValues);
+        // assigning values
+        Long userId = Long.valueOf(elementValues.get(0));
+        String textField = elementValues.get(1);
+        Long journeyId = Long.valueOf(elementValues.get(2));
+        User user = userRepository.findById(userId).get();
+        Journey journey = journeyRepository.findById(journeyId).get();
+//        String firstName = elementValues.get(0);
+//        String lastName = elementValues.get(1);
+//        String username = elementValues.get(2);
+//        String password = encoder.encode(elementValues.get(3));
+//        String gender = elementValues.get(4);
+//        String email = elementValues.get(5);
+//        String phoneNumber =  elementValues.get(6);
+//        Long favouriteTeamId = Long.valueOf(elementValues.get(7));
+//        String role = "USER";
+        return new Post(textField, user, journey);
+    }
+    public UserGivesReferenceToUser parseRefJson(String reference)throws JSONException {
+        ArrayList<String> elementValues = parseAllElements(reference);
+        for (String s:elementValues
+        ) {
+            System.out.println(s);
+        }
+        String referenceType = elementValues.get(2);
+        Long giverId = Long.valueOf(elementValues.get(0));
+        Long receiverId = Long.parseLong((elementValues.get(1)));
+        User user = userRepository.findById(receiverId).get();
+        String textField = (elementValues.get(6));
+        LocalDate time = LocalDate.now();
+        String giverRole = elementValues.get(4);
+        String receiverRole = elementValues.get(5);
+        return new UserGivesReferenceToUser(giverId,referenceType, time, giverRole, receiverRole, textField, user);
+    }
     public ArrayList<String> parseAllElements(String stringToParse)throws JSONException {
-
         ArrayList<String> elementValues = new ArrayList<>();
-
         stringToParse = stringToParse.replace("\"", "");
         stringToParse = stringToParse.replace("}", "");
         String [] keyValuePairs = stringToParse.split(",");
-
         for (int i = 0; i < keyValuePairs.length; i++) {
             String[] values = keyValuePairs[i].split(":");
             elementValues.add(values[1]);
             System.out.println("Element" + i + " : " + elementValues.get(i));
         }
-
         return elementValues;
     }
 }
-
-
--- src/main/java/com/example/EventCarPoolAPI/UserGivesReferenceToUser.java
+++ src/main/java/com/example/EventCarPoolAPI/UserGivesReferenceToUser.java
@@ -1,12 +1,8 @@
 package com.example.EventCarPoolAPI;
-
-
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import javax.persistence.*;
 import java.time.LocalDate;
-
 @Entity
 @Table (name = "User_Gives_Reference_To_User")
 public class UserGivesReferenceToUser {
@@ -23,81 +19,68 @@ public class UserGivesReferenceToUser {
     private String giverRole;
     private String receiverRole;
     private String textField;
-
-
     public UserGivesReferenceToUser() {
     }
-
-    public UserGivesReferenceToUser(Long giverId, String referenceType, LocalDate time, String giverRole, String receiverRole, String textField) {
+    public UserGivesReferenceToUser(Long giverId, String referenceType, LocalDate time, String giverRole, String receiverRole, String textField, User userReceiving) {
         this.giverId = giverId;
         this.referenceType = referenceType;
         this.time = time;
         this.giverRole = giverRole;
         this.receiverRole = receiverRole;
         this.textField = textField;
+        this.userReceiving = userReceiving;
     }
+
 
     public Long getReferenceId() {
         return referenceId;
     }
-
     public void setReferenceId(Long referenceId) {
         this.referenceId = referenceId;
     }
-
     public Long getGiverId() {
         return giverId;
     }
-
     public void setGiverId(Long giverId) {
         this.giverId = giverId;
     }
-
     public User getUserReceiving() {
         return userReceiving;
     }
-
     public void setUserReceiving(User userReceiving) {
         this.userReceiving = userReceiving;
     }
-
     public String getReferenceType() {
         return referenceType;
     }
-
     public void setReferenceType(String referenceType) {
         this.referenceType = referenceType;
     }
-
     public LocalDate getTime() {
         return time;
     }
-
     public void setTime(LocalDate time) {
         this.time = time;
     }
-
     public String getGiverRole() {
         return giverRole;
     }
-
     public void setGiverRole(String giverRole) {
         this.giverRole = giverRole;
     }
-
     public String getReceiverRole() {
         return receiverRole;
     }
-
     public void setReceiverRole(String receiverRole) {
         this.receiverRole = receiverRole;
     }
-
     public String getTextField() {
         return textField;
     }
-
     public void setTextField(String textField) {
         this.textField = textField;
     }
 }
+
+
+
